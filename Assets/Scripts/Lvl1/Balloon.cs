f6f417f09c0a210022810b7a3995581f47cee8cc
AndresGS <andres.granero.tik@gmail.com>
2025-11-11 12:21:54 +0100

    Fix pool prefabs

--- Assets/Scripts/Lvl1/Balloon.cs
+++ Assets/Scripts/Lvl1/Balloon.cs
@@ -11,14 +11,18 @@ public class Balloon : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        print(other.tag);
+        
         //porque si no me detecta más de uno como van muy rápidas las balas y son varias pues eso  
         if (exploded)
         {
+            print("ex");
             return;
         }
         if (other.CompareTag("BulletP"))
         {
             exploded = true;
+            print("explo");
             Manager1.instance.BalloonExplode();
             
             Destroy(gameObject);
